f81279847199dabb286b11b61e750fda5d1ca50d
agrdino <tlmtri.dino@gmail.com>
2026-08-06 22:10:27 +0700

    add simple controller

---
+++ Assets/_Scripts/Controller/HandController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Readers;
+
+namespace _Scripts.Controller
+{
+    public class HandController : MonoBehaviour
+    {
+        #region ----- Component Config -----
+
+        [SerializeField]
+        private XRInputValueReader<float> _triggerInput = new XRInputValueReader<float>("Trigger");
+
+        #endregion
+
+        #region ----- Event ----
+
+        public event Action<float> onPress; 
+
+        #endregion
+        
+        private void OnEnable()
+        {
+            _triggerInput?.EnableDirectActionIfModeUsed();
+        }
+
+        private void Update()
+        {
+            onPress?.Invoke(_triggerInput.ReadValue());
+        }
+
+        private void OnDisable()
+        {
+            _triggerInput?.DisableDirectActionIfModeUsed();
+        }
+    }
+}
